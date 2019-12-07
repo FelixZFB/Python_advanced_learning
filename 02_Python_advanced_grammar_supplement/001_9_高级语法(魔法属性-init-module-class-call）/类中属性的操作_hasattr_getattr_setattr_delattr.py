bb9e1f21295ddae7a6f205742d6c3bad943e2452
Felix Zhang <18200116656@qq.com>
2019-12-07 21:28:07 +0800

    Daily commit

---
+++ 02_Python_advanced_grammar_supplement/001_9_高级语法(魔法属性-init-module-class-call）/类中属性的操作_hasattr_getattr_setattr_delattr.py
@@ -0,0 +1,25 @@
+# -*- coding:utf-8 -*-
+# project_xxx\venv\Scripts python
+
+'''
+Author: Felix
+Email: xiashubai@gmail.com
+Blog: https://blog.csdn.net/u011318077
+Date: 2019/12/7 17:49
+Desc:
+'''
+
+class function_demo(object):
+    name = 'demo'
+    def run(self):
+        return "hello function"
+functiondemo = function_demo()
+res1 = hasattr(functiondemo, "name") # 判断对象是否有name属性，True
+res2 = hasattr(functiondemo, "run") # 判断对象是否有run方法，True
+res3 = hasattr(functiondemo, "age") # 判断对象是否有age属性，False
+print(res1, res2, res3)
+
+# 删除属性
+res4 = delattr(function_demo, 'name')
+res5 = hasattr(functiondemo, "name")
+print(res4, res5)
